20186697bdab915bcaaba7c08bfff6fa9728feb9
soleilsolis <fukuman23@gmail.com>
2022-08-09 16:04:43 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db7hjcyqirng4c' );
+define( 'DB_NAME', 'mcha' );
 
 /** Database username */
-define( 'DB_USER', 'unxehwbzvnjz0' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '9jgna2brzcys' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', '127.0.0.1' );
